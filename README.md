3c476d1683af94781923716a0dad61eba0b861e8
Akihide Tsue <akihide.tsue@gmail.com>
2019-11-14 12:30:51 +0900

    “アプリ作成”

---
+++ README.md
@@ -0,0 +1,13 @@
+## READEME
+
+### このアプリについて
+ターミナル上で動くRubyアプリです  
+Livedoor が提供しているお天気WebサービスAPIを取得しています  
+Ruby 2.5.1  
+作成者 津江明秀(TECH::EXPERT63期 なんば校)
+天気データ：http://weather.livedoor.com/forecast/rss/primary_area.xml
+
+### 使い方
+1.まずはダウンロード
+2.ターミナル：tenki.rbのディレクトリで $ruby tenki.rb  
+3.東京か大阪の天気をチェックできます
---
+++ tenki.rb
@@ -0,0 +1,61 @@
+require 'uri'
+require 'net/http'
+require 'json'
+require 'date'
+
+def read_tenki
+  puts "[1]東京の天気"
+  puts "[2]大阪の天気"
+  input = gets.to_i
+    if input == 1 then
+      tenki_tokyo
+    elsif input == 2 then
+      tenki_osaka
+    else
+      exception
+      read_tenki
+    end
+end
+
+def tenki_tokyo
+  tokyo = "http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"
+  info(tokyo)
+end
+
+def tenki_osaka
+  osaka = "http://weather.livedoor.com/forecast/webservice/json/v1?city=270000"
+  info(osaka)
+end
+
+def info(place)
+  uri = URI.parse(place)
+  res = Net::HTTP.get(uri)
+  res = JSON.parse(res)
+  forecasts = res["forecasts"]
+  tomorrow_forecast = forecasts[1]
+  telop = tomorrow_forecast["telop"]
+  location = res["location"]["city"]
+  day = Date.today.day + 1
+  puts "#{day}日、#{location}は#{telop}でしょう"
+end
+
+def end_program
+  exit
+end
+
+def exception
+  puts "入力された値は無効な値です"
+end
+
+while true do           # メニューの表示
+  puts "[1]天気を見る"
+  puts "[0]アプリを終了する"
+  input = gets.to_i
+  if input == 1 then    # read_tenkiメソッドの呼び出し
+    read_tenki
+  elsif input == 0 then
+    end_program         # end_programメソッドの呼び出し
+  else
+    exception           # exceptionメソッドの呼び出し
+  end
+end
