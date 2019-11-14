a0de4fdbc77195c0fd3e713c53bea9656f47e3b7
Akihide Tsue <akihide.tsue@gmail.com>
2019-11-15 08:42:38 +0900

    “5都市に追記、digでデータなし表示”

--- tenki.rb
+++ tenki.rb
@@ -2,7 +2,6 @@ require 'uri'
 require 'net/http'
 require 'json'
 require 'date'
-
 line = "----------------------"
 
 def tenki_tokyo
@@ -15,11 +14,25 @@ def tenki_osaka
   info(osaka)
 end
 
+def tenki_nagoya
+  nagoya = "http://weather.livedoor.com/forecast/webservice/json/v1?city=230010"
+  info(nagoya)
+end
+
+def tenki_sapporo
+  sapporo = "http://weather.livedoor.com/forecast/webservice/json/v1?city=016010"
+  info(sapporo)
+end
+
+def tenki_okinawa
+  okinawa = "http://weather.livedoor.com/forecast/webservice/json/v1?city=471010"
+  info(okinawa)
+end
+
 def info(place)
   uri = URI.parse(place)
   res = Net::HTTP.get(uri)
   res = JSON.parse(res)
-
   location = res["location"]["city"]
   today = Date.today.day
   tommorow = Date.today.day + 1
@@ -27,16 +40,18 @@ def info(place)
 
   today_forecast = forecasts[0]
   today_telop = today_forecast["telop"]
-  today_max = today_forecast["temperature"]["max"]["celsius"]
+  today_max = today_forecast.dig(*%w[temperature max celsius]) || "データなし"
+  today_min = today_forecast.dig(*%w[temperature min celsius]) || "データなし"
 
   tomorrow_forecast = forecasts[1]
   tomorrow_telop = tomorrow_forecast["telop"]
-  tomorrow_min = tomorrow_forecast["temperature"]["min"]["celsius"]
-  tomorrow_max = tomorrow_forecast["temperature"]["max"]["celsius"]
+  tomorrow_max = tomorrow_forecast.dig(*%w[temperature max celsius]) || "データなし"
+  tomorrow_min = tomorrow_forecast.dig(*%w[temperature min celsius]) || "データなし"
 
   puts "  #{location}の天気：
-  今日(#{today}日)  ：#{today_telop}、最高気温:#{today_max}、最低気温:データなし
+  今日(#{today}日)  ：#{today_telop}、最高気温:#{today_max}、最低気温:#{today_min}
   明日(#{tommorow}日)  ：#{tomorrow_telop}、最高気温:#{tomorrow_max}、最低気温:#{tomorrow_min}"
+
   puts "----------------------"
 end
 
@@ -48,16 +63,17 @@ def exception
   puts "入力された値は無効な値です"
 end
 
-while true do           # メニューの表示
-  puts "\n[1]東京の天気を見る\n[2]大阪の天気を見る\n[0]アプリを終了する"
+while true do  # メニューの表示
+  puts "天気を確認したい地名を選択して下さい。"
+  puts "\n[1]東京\n[2]大阪\n[3]名古屋\n[4]札幌\n[5]那覇\n[0]アプリを終了する"
   puts "\nLivedoor お天気Webサービス API ：\nhttp://weather.livedoor.com/forecast/rss/primary_area.xml"
   puts line
-
   input = gets.to_i
-    if input == 1 then
-      tenki_tokyo
-    elsif input == 2 then
-      tenki_osaka
+    if input == 1 then tenki_tokyo
+    elsif input == 2 then tenki_osaka
+    elsif input == 3 then tenki_nagoya
+    elsif input == 4 then tenki_sapporo
+    elsif input == 5 then tenki_okinawa
     elsif input == 0 then
       end_program         # end_programメソッドの呼び出し
     else
